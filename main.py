cfbcbcfbf8e7e09f35151d656053055f59fd6878
Zach <36242412+zach1020@users.noreply.github.com>
2023-02-20 15:38:09 -0600

    Updated main.py
    Updated main to include speech-to-text functionality.

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from chat import chat
 from tts import speak
+from stt import stt
 
 # main.py
-
+''' Old main function
 def main():
 	prompt = ''
 	while prompt != 'quit':
 		prompt = input('> ')
 		speak(chat(prompt))
+'''
+
+def main():
+	while True:
+		prompt = stt()
+		speak(chat(prompt))
+
 
 main()
