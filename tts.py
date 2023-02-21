9d39a238729fa2c5e8000db1b04d06759b426c7e
Zach <36242412+zach1020@users.noreply.github.com>
2023-02-20 22:18:44 -0600

    Updated tts.py
    Updated from an API to a simple 'say' command in the terminal

--- tts.py
+++ tts.py
@@ -1,18 +1,4 @@
-from gtts import gTTS
-
-import os 
-
-import vlc
-import time
+from os import system
 
 def speak(my_text):
-	language = 'en'
-
-	my_obj = gTTS(text=my_text, lang=language, slow=False)
-
-	my_obj.save('speak.mp3')
-
-	p = vlc.MediaPlayer('speak.mp3')
-	p.play()
-	duration = p.get_length()
-	time.sleep(duration / 1000) # duration in milliseconds
+	system(f"say {my_text}")
